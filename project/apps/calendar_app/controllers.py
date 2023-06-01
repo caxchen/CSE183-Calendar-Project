849ef47e4dee741e06390e7aaa1e10ff866dc113
dhsieh4 <dhsieh4@ucsc.edu>
2023-06-01 15:59:10 -0700

    Added py4web code and html for creating events to the database as well as editing, also updated pages to use Bulma

--- project/apps/calendar_app/controllers.py
+++ project/apps/calendar_app/controllers.py
@@ -25,22 +25,38 @@ session, db, T, auth, and tempates are examples of Fixtures.
 Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
 """
 
-from py4web import action, request, abort, redirect, URL
+from py4web import action, request, abort, redirect, URL, HTTP
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
+from py4web.utils.url_signer import URLSigner
+from py4web.utils.form import Form, FormStyleBulma
 
 
 @action("index")
 @action.uses("index.html", auth.user, T)
 def index():
     user = auth.get_user()
-    message = T("Hello {first_name}".format(**user) if user else "Hello")
+    message = T("{first_name}'s calendar".format(**user) if user else "Hello")
     actions = {"allowed_actions": auth.param.allowed_actions}
     return dict(message=message, actions=actions)
 
-"""
-@action('index')
-@action.uses('index.html', db, auth.user)
-def index():
-    return dict(
-    )"""
+@action("create_event", method=["GET", "POST"])
+@action.uses("create_event.html", db, session, auth.user)
+def create_event():
+    form = Form(db.event, csrf_session=session, formstyle=FormStyleBulma)
+    if form.accepted:
+        redirect(URL('index'))
+    return dict(form=form)
+
+@action('edit_event/<id:int>', method=["GET", "POST"])
+@action.uses('edit_event.html', db, session, auth.user)
+def edit_event(id=None):
+    assert id is not None
+    edit_event = db.event[id]
+    # if edit_event.created_by != auth.user(): raise HTTP(400)
+    if edit_event is None:
+        redirect(URL('index'))
+    form = Form(db.event, record=edit_event, deletable=False, csrf_session=session, formstyle=FormStyleBulma)
+    if form.accepted:
+        redirect(URL('index'))
+    return dict(form=form)
