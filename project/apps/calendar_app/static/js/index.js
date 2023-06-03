bb025ccbfe4b508ffb7e6492eacf84eb8c5c938c
Sydney DeHaro <sdeharo@ucsc.edu>
2023-06-02 18:08:22 -0700

    Events on calendar now clickable and viewable.

--- project/apps/calendar_app/static/js/index.js
+++ project/apps/calendar_app/static/js/index.js
@@ -6,24 +6,24 @@ function main() {
     document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth' //dayGridDay
+          initialView: 'dayGridMonth', //dayGridDay
+          eventClick: function(info) {
+            window.location.href =`../view_event/${info.event.id}`;
+          }
+          
         });
-
-        /*calendar.addEvent({
-            id: 'test1',
-            title: 'test event',
-            start: '2023-06-03T14:30:00'
-        })*/
+        
         calendar.render();
 
         axios.get("../get_events").then(function(r) {
             if (r.data.events) {
                 for (let i=0; i<r.data.events.length; i++) {
+                    let event_id = r.data.events[i].id;
                     let name = r.data.events[i].name;
                     let time = r.data.events[i].event_time;
                     let description = r.data.events[i].description;
                     calendar.addEvent({
-                        //id: 'test1',
+                        id: event_id,
                         title: name,
                         start: time,
                         extendedProps: {
@@ -34,7 +34,6 @@ function main() {
             }
         });
     });
-    console.log("called main!", Math.random());
 }
 
 main();
