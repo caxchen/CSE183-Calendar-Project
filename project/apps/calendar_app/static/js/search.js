5c3913f962d846560d5b4c27400f1d1f42468f1f
Sydney DeHaro <sdeharo@ucsc.edu>
2023-06-07 17:31:29 -0700

    Implemented clear button for event search.

--- project/apps/calendar_app/static/js/search.js
+++ project/apps/calendar_app/static/js/search.js
@@ -11,10 +11,12 @@ function init() {
     self.data.events = [];
     self.data.text = ""
 
+    // Get events
     axios.get("../get_events").then(function(r){
         self.vue.events = r.data.events;
     });
     
+    // Search function
     self.methods.filter_events = function() {
       if(self.vue.text.length>0) {
           fetch("/calendar_app/get_events?text="+encodeURIComponent(self.vue.text))
@@ -24,6 +26,20 @@ function init() {
           }).catch(function(error) {
             console.error("Error fetching events:", error)});
         }
+    };
+
+    // Clear function
+    self.methods.clear = function() {
+        // Empty text variables to clear input
+        self.data.text = "";
+        self.vue.text = "";
+        // Refetch all events
+        fetch("/calendar_app/get_events?text="+encodeURIComponent(self.vue.text))
+        .then(r=>r.json())
+        .then(function(data){
+          self.vue.events = data.events;
+        }).catch(function(error) {
+          console.error("Error fetching events:", error)});
     };
 
     self.vue = new Vue({el:"#vue", data: self.data, methods: self.methods});
